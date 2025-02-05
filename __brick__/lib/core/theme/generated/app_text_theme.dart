53872044289ae6de53887ed1fe1431309ff84493
Andre Wijaya <andre8.aw6@gmail.com>
2025-02-05 16:34:45 +0700

    update

--- __brick__/lib/core/theme/generated/app_text_theme.dart
+++ __brick__/lib/core/theme/generated/app_text_theme.dart
@@ -5,11 +5,11 @@ import 'app_font.dart';
 
 class AppTextTheme {
   {{#colors}}
-  static const {{name}} = TextStyle(color:AppColor.{{name}}, fontFamily: "{{fontFamily}}",);
+  static const {{name}} = TextStyle(fontFamily: "{{fontFamily}}",color:AppColor.{{name}}, );
   {{/colors}}
-  
+
   {{#textStyles}}
-  static const {{name}} = TextStyle(fontSize : {{size}}, fontFamily: "{{fontFamily}}",);
+  static const {{name}} = TextStyle(fontFamily: "{{fontFamily}}",fontSize : {{size}},);
   {{/textStyles}}
 
   
