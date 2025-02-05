b7284016cc21ce5620e13df46c51d2d426d87a26
Andre Wijaya <andre8.aw6@gmail.com>
2025-02-05 16:24:25 +0700

    update

--- __brick__/lib/core/theme/generated/app_text_theme.dart
+++ __brick__/lib/core/theme/generated/app_text_theme.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/widgets.dart';
 
+import 'app_color.dart';
 import 'app_font.dart';
 
 class AppTextTheme {
   {{#textStyles}}
   static const {{name}} = TextStyle(fontSize : {{size}}, fontFamily: "{{fontFamily}}",);
   {{/textStyles}}
+
+  {{#colors}}
+  static const {{name}} = TextStyle( color:AppColor.{{name}}, fontFamily: "{{fontFamily}}",);
+  {{/colors}}
   
 }
