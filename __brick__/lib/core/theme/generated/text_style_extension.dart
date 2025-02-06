8b6ae095f4ec2a790e3cd54a138fb410eb36a2ee
Andre Wijaya <andre8.aw6@gmail.com>
2025-02-06 09:12:28 +0700

    update

--- __brick__/lib/core/theme/generated/text_style_extension.dart
+++ __brick__/lib/core/theme/generated/text_style_extension.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-{{#screen_util}}
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-{{/screen_util}}
 
 import 'app_color.dart';
 import 'app_font.dart';
-import 'num_extension.dart';
+
 
 extension GenThemeTextStyleExtension on TextStyle {
   
@@ -24,13 +21,5 @@ extension GenThemeTextStyleExtension on TextStyle {
     fontSize: AppFont.{{name}},
   );
   {{/fontSizes}}
-
-  {{#screen_util}}
-  {{#fontSizes}}
-  TextStyle {{name}}Orientation(BuildContext context) => copyWith(
-    fontSize: AppFont.{{name}}Basic.orientationSp(context),
-  );
-  {{/fontSizes}}
-  {{/screen_util}}
 
 }
